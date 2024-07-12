b5be6a3f0d9594367bfb2d2cde2280d25e8d5f42
Felipe Alves <profelipealves@outlook.com>
2024-07-11 21:04:04 -0300

    chore: added react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,18 @@
-import { CircularProgress } from "@mui/material";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import ComicList from "./components/ComicList";
-
-interface Comic {
-  id: number;
-  title: string;
-  description: string;
-  thumbnail: {
-    path: string;
-    extension: string;
-  };
-}
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import './App.css';
+import CharacterList from './components/CharacterList';
 
 const App = () => {
-  const [comics, setComics] = useState<Comic[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [queryClient] = useState(() => new QueryClient());
 
-  useEffect(() => {
-    const fetchComics = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await axios.get(
-          "http://gateway.marvel.com/v1/public/comics",
-          {
-            params: {
-              apikey: "676a34a3184dfbcef1093ea17f4113dc",
-              orderBy: "title",
-              limit: 20,
-            },
-          }
-        );
-
-        setComics(response.data.data.results);
-      } catch (error) {
-        console.error(error);
-      }
-
-      setIsLoading(false);
-    };
-
-    fetchComics();
-  }, []);
-
-  if (isLoading) {
-    return <CircularProgress />;
-  }
-
-  return <ComicList comics={comics} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CharacterList />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
