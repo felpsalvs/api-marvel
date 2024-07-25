d45075574d5f520601a707abccfee334bbebe88a
Felipe Alves <profelipealves@outlook.com>
2024-07-25 18:21:24 -0300

    chore: added logo marvel

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import './App.css';
 import CharacterList from './components/CharacterList';
+import logo from './assets/marvel-logo.svg';
 
 const App = () => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <CharacterList />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <>
+      <div className="flex justify-center">
+        <img
+          className="w-64 md:w-80 pt-4 pb-10 transition-all duration-500"
+          src={logo}
+          alt="Marvel"
+        />
+      </div>
+      <QueryClientProvider client={queryClient}>
+        <CharacterList />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
   );
 };
 
