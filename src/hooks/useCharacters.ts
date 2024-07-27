ba0245aaf826fdfa5b4bd445d801ae56b1184c90
Felipe Alves <profelipealves@outlook.com>
2024-07-27 11:41:17 -0300

    update interface characters

--- src/hooks/useCharacters.ts
+++ src/hooks/useCharacters.ts
@@ -1,31 +1,43 @@
 import axios, { AxiosError } from 'axios';
 import { useQuery } from 'react-query';
-import { Character } from '../store/useCharactersStore';
 
-const MARVEL_API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
+export interface Character {
+  id: number;
+  name: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+}
 
-const useCharacters = () => {
-  return useQuery<Character[] | undefined, AxiosError>({
-    queryKey: 'characters',
-    queryFn: fetchCharacters,
+interface CharactersData {
+  characters: Character[];
+  total: number;
+}
+
+const useCharacters = (offset: number) => {
+  return useQuery<CharactersData, AxiosError>({
+    queryKey: ['characters', offset],
+    queryFn: () => fetchCharacters(offset),
     onError: error => {
       console.log('Failed to fetch characters:', error.message);
     },
-   staleTime: 12 * 60 * 60 * 1000, // It is unlikely that the information will change in less than 12 hours.
+    staleTime: 12 * 60 * 60 * 1000, // It is unlikely that the information will change in less than 12 hours.
   });
 };
 
-const fetchCharacters = async () => {
+const fetchCharacters = async (offset?: number) => {
+  const MARVEL_API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
   try {
     const { data } = await axios.get(
-      `https://gateway.marvel.com:443/v1/public/characters?apikey=${MARVEL_API_KEY}`
+      `https://gateway.marvel.com:443/v1/public/characters?apikey=${MARVEL_API_KEY}&offset=${offset}`
     );
-    const characters = data.data.results.map((character: Character) => ({
+    const characters = data?.data.results.map((character: Character) => ({
       id: character.id,
       name: character.name,
       thumbnail: character.thumbnail,
     }));
-    return characters;
+    return { characters, total: data.data.total };
   } catch (error) {
     const axiosError = error as AxiosError;
     throw new Error(`Failed to fetch properties: ${axiosError.message}`);
