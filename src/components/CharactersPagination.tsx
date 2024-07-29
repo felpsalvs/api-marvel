a61c5afa73942f839452fb67a577324a5a8bbc63
Felipe Alves <profelipealves@outlook.com>
2024-07-29 18:30:48 -0300

    update CharactersPagination.tsx

--- src/components/CharactersPagination.tsx
+++ src/components/CharactersPagination.tsx
@@ -3,6 +3,11 @@ import Stack from '@mui/material/Stack';
 import { styled } from '@mui/system';
 import useCharacters from '../hooks/useCharacters';
 import usePagination from '../hooks/usePagination';
+import {
+  usePaginationStore,
+  setPage,
+  setOffset,
+} from '../store/usePaginationStore';
 
 const CustomPagination = styled(Pagination)(({ theme }) => ({
   ul: {
@@ -12,14 +17,14 @@ const CustomPagination = styled(Pagination)(({ theme }) => ({
   },
 }));
 
-interface Props {
-  onPageChange: (event: React.ChangeEvent<unknown>, value: number) => void;
-}
 
-const CharactersPagination = ({ onPageChange }: Props) => {
+const CharactersPagination = () => {
   const PAGINATION_OFFSET = 20;
 
-  const { page, setPage, offset, setOffset } = usePagination();
+  const { page, offset } = usePaginationStore(state => ({
+    page: state.page,
+    offset: state.offset,
+  }));
 
   const { data: characterData } = useCharacters(offset);
 
@@ -27,7 +32,6 @@ const CharactersPagination = ({ onPageChange }: Props) => {
     const newOffset = value > 1 ? (value - 1) * 20 : 0;
     setPage(value);
     setOffset(newOffset);
-    onPageChange(event, newOffset);
   };
 
   return characterData ? (
